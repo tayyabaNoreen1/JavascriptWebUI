946fec666f9db300e639beac4a04c35b5fda3c93
tayyabaNoreen1 <tayyaba1.noreen@gmail.com>
2024-01-04 20:04:20 +0000

    checking

---
+++ Intro to javascript/tau-intro-to-js/05-objects.js
@@ -0,0 +1,32 @@
+const cookie = {
+  name: 'Chocolate Chip',
+  isGlutenFree: false,
+  '+yummy': true,
+  eatCookie: function () {
+    console.log('I am eating ' + this.name);
+  },
+};
+console.log(cookie);
+console.log(cookie.name);
+cookie.eatCookie();
+cookie.name = 'party biscuit';
+cookie.isGlutenFree = true;
+console.log(cookie);
+
+console.log(cookie['+yummy']);
+
+class Cookie {
+  constructor(name, isGlutenFree) {
+    this.name = name;
+    this.isGlutenFree = isGlutenFree;
+  }
+  eatCookie() {
+    console.log('I am enjoying ' + this.name);
+  }
+}
+const myCookie = new Cookie('sooper', false);
+
+console.log(myCookie);
+console.log(myCookie.name);
+console.log(myCookie['name']);
+myCookie.eatCookie();
---
+++ Intro to javascript/tau-intro-to-js/07-while-loops.js
@@ -0,0 +1,17 @@
+let password = 'tomorrow';
+let userGuess = '';
+
+// while (userGuess != password) {
+//   userGuess = prompt('Please enter your pssword.');
+// }
+
+// alert('Login Successfull.');
+
+let x = 5;
+let fact = 1;
+do {
+  fact = fact * x;
+  x--;
+} while (x > 0);
+
+console.log('Factorial = ' + fact);
